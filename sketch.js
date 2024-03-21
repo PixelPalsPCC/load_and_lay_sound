a885ed898ac6f94c1caa02bacb8df10b23fda691
George McKinney <george@hookstream.com>
2024-03-21 01:47:02 -0700

    added files

---
+++ sketch.js
@@ -0,0 +1,20 @@
+// source https://p5js.org/examples/sound-load-and-play-sound.html
+
+let song;
+
+function setup() {
+  song = loadSound('assets/lucky_dragons_-_power_melody.mp3');
+  createCanvas(720, 200);
+  background(255, 0, 0);
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+    background(255, 0, 0);
+  } else {
+    song.play();
+    background(0, 255, 0);
+  }
+}
